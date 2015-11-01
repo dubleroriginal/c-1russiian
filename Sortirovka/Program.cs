a65c738ee8453d7cc508822e4e72b6539116b949
dubleroriginal <doublureruss@gmail.com>
2015-11-01 09:34:22 +0200

    Update Program.cs

--- Sortirovka/Program.cs
+++ Sortirovka/Program.cs
@@ -1,91 +1,67 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+namespace Sortirovka {
+	class Program {
+		static void Main(string[] args) {
+			int sizeMas;
+			int repeatCycle = 0;
+			do {
+				Console.WriteLine("vved razm mas");
+				sizeMas = Convert.ToInt32(Console.ReadLine());
+				CreateMas(sizeMas);
+				Console.WriteLine("for out press 1");
+				repeatCycle = Convert.ToInt32(Console.ReadLine());
+				Console.Clear();
+			} while (repeatCycle != 1);
+		}
+		static void CreateMas(int razMas) {
+			Random rand = new Random();
+			int[] Mas = new int[razMas];
+			for (int i = 0; i < Mas.Length; i++) {
+				Mas[i] = rand.Next(-razMas, razMas);
+			}
 
-namespace Sortirovka
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+			Console.WriteLine("Mas was created");
+
+			PrintMas(Mas);
+			Console.WriteLine("Mas was sorted");
+			quickSort(Mas, 0, Mas.Length - 1);
+			PrintMas(Mas);
+
+		}
+		static void PrintMas(int[] mas) {
+			for (int i = 0; i < mas.Length; i++) {
+				Console.Write(mas[i] + " ");
+				if (i == mas.Length - 1) Console.WriteLine(" ");
+			}
+		}
 
 
-            int N;// razmer mas
-            int ou=0;
 
-            do
-            {
-            Console.WriteLine("vv razm mas");
-            N = Convert.ToInt32(Console.ReadLine());
+		static void quickSort(int[] mas, int left, int right) {
+			int prom;
+			int x = mas[left + (right - left) / 2];
 
-            CreateMas(N);
+			int i = left;
+			int j = right;
 
-            Console.WriteLine("for out press 1");
-             ou = Convert.ToInt32( Console.ReadLine());
-             Console.Clear();
-              }while(ou!=1);
-       }
+			while (i <= j) {
+				while (mas[i] < x) i++;
+				while (mas[j] > x) j--;
+				if (i <= j) {
+					prom = mas[i];
+					mas[i] = mas[j];
+					mas[j] = prom;
+					i++;
+					j--;
+				}
+			}
+			if (i < right) quickSort(mas, i, right);
 
-
-        static void CreateMas(int razMas)
-        {
-            Random rand = new Random();
-
-            int[] Mas = new int[razMas];
-
-            for (int i = 0; i < Mas.Length; i++)
-            {
-                Mas[i] = rand.Next(-razMas, razMas);
-            }
-
-            Console.WriteLine("Mas was created");
-
-            Out(Mas);
-            Console.WriteLine("Mas was sorted");
-            quickSort(Mas,0,Mas.Length-1);
-            Out(Mas);
-
-        }
-        static void Out(int[] mas)
-        {
-            for (int i = 0; i < mas.Length; i++)
-            {
-                Console.Write(mas[i] + " ");
-                if (i == mas.Length-1)
-                    Console.WriteLine(" ");
-            }
-        }
-
-        
-
-        static void quickSort(int[] mas, int left, int right)
-        {
-            int temp;
-            int x = mas[left + (right - left) / 2];
-           
-            int i = left;
-            int j = right;
-           
-            while (i <= j)
-            {
-                while (mas[i] < x) i++;
-                while (mas[j] > x) j--;
-                if (i <= j)
-                {
-                    temp = mas[i];
-                    mas[i] = mas[j];
-                    mas[j] = temp;
-                    i++;
-                    j--;
-                }
-            }
-            if (i < right)
-                quickSort(mas, i, right);
-
-            if (left < j)
-                quickSort(mas, left, j);
-        }
-    }
+			if (left < j) quickSort(mas, left, j);
+		}
+	}
 }
